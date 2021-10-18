3770309408a9725c647bf2da7904d2daa08fdda3
Mac Rowe <m.rowe42@gmail.com>
2021-10-18 13:13:23 -0400

    Import ProductItem

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,15 +1,24 @@
 import React from 'react'; 
 import { FlatList } from 'react-native'; 
-import { Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts); 
     return (
         <FlatList 
-        data={products} 
-        keyExtractor={item => item.id} 
-        renderItem={itemData => <Text>{itemData.item.title}</Text>} 
+            data={products} 
+            keyExtractor={item => item.id} 
+            renderItem={itemData => (
+                <ProductItem 
+                    image={itemData.item.imageUrl} 
+                    title={itemData.item.title} 
+                    price={itemData.item.price} 
+                    onViewDetail={() => {}} 
+                    onAddToCart={() => {}}
+                />
+            )} 
         />
     );
 }; 
