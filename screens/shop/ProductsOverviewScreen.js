94d23c3b1859dea76a0a6113c2333a4c849b3b66
Mac Rowe <m.rowe42@gmail.com>
2021-10-18 11:37:21 -0400

    Set up initial FlatList render

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import { View, Text, StyleSheet, FlatList } from 'react-native'; 
+
+const ProductsOverviewScreen = props => {
+    return <FlatList /> 
+}; 
+
+
+export default ProductsOverviewScreen; 
