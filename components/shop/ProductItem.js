e5514a494c9dcad6ff53daa52fd33a3cade81420
Mac Rowe <m.rowe42@gmail.com>
2021-10-18 13:13:46 -0400

    Style product cards

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import Colors from '../../constants/Colors'
 
 const ProductItem = props => {
     return (
         <View style={styles.product}>
-            <Image source={{uri: props.image}} />
-            <Text>{props.title}</Text>
-            <Text>${props.price.toFixed(2)}</Text>
-            <View>
-                <Button title="View Details" />
-                <Button title="View Cart" />
+
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{uri: props.image}} />
             </View>
+
+            <View style={styles.details}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+
+            <View style={styles.actions}>
+                <Button 
+                    color={Colors.primary} 
+                    title="View Details" 
+                    onPress={props.onViewDetail} 
+                />
+                <Button 
+                    color={Colors.primary} 
+                    title="View Cart" 
+                    onPress={props.onAddToCart}
+                />
+            </View>
+
         </View>
     ); 
 ß}; 
@@ -26,6 +43,37 @@ const styles = StyleSheet.create({
         backgroundColor: 'white', 
         height: 300,
         margin: 20
+    },
+    imageContainer: {
+        width: '100%',
+        height: '60%', 
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10, 
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginVertical: 4
+    },
+    price: {
+        fontSize: 14,
+        color: '#888'
+    },
+    actions: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '25%',
+        paddingHorizontal: 20
     }
 });
 
