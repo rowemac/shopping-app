0eb2b8dd42d14a41dd21aba751d38653f4cd5596
Mac Rowe <m.rowe42@gmail.com>
2021-10-20 12:30:57 -0400

    Implement fetchFonts function to work upon App loading

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux'; 
 import { AppLoading } from 'expo';
@@ -13,7 +13,27 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/Open-Sans-Regular.ttf'), 
+    'open-sans-bold': require('./assets/fonts/Open-Sans-Bold.ttf')
+  })
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false); 
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => {
+          setFontLoaded(true);
+        }} 
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
