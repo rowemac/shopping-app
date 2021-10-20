57dd0885778d51042c4aab5ef0d8fc151fbc0b47
Mac Rowe <m.rowe42@gmail.com>
2021-10-20 14:34:22 -0400

    Fix variable names

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,41 +1,36 @@
-import { ADD_TO_CART } from "../actions/cart";
-import CartItem from "../../models/cart-item";
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
 
 const initialState = {
-    items = {},
-    totalAmount = 0
-}; 
+  items: {},
+  totalAmount: 0
+};
 
-export default (state = initialState, action) => { 
-    switch (action.type) {
-        case ADD_TO_CART:
-            const addedProduct = action.product; 
-            const prodPrice = addedProduct.price; 
-            const prodTitle = addedProduct.title;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
 
-            let updatedOrNewCartItem; 
-            
-            if (items[addedProduct.id]) {
-                // already have item in the cart
-                updatedOrNewCartItem = newCartItem(
-                    state.items[addedProduct.id].quantity + 1,
-                    prodPrice, 
-                    prodTitle,
-                    state.items[addedProduct.id].sum + prodPrice
-                );
-                return {
-                    ...state, 
-                    items: { ...state.items, [addedProduct.id]: updatedCartItem },
-                    totalAmount: state.totalAmount + prodPrice
-                };
-            } else {
-                updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice); 
-            }
-            return {
-                ...state, // only works if you need to add other attributes to the state
-                items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem }, 
-                totalAmount: state.totalAmount + prodPrice
-            };
-        }
-    return state; 
+      let updatedOrNewCartItem;
+
+      if (state.items[addedProduct.id]) {
+        // already have the item in the cart
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice
+      };
+  }
+  return state;
 };
