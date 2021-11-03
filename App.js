620b0e4dd404be0eaac50bca63a43dab7835cf9a
Mac Rowe <m.rowe42@gmail.com>
2021-11-03 11:51:59 -0400

    Import ordersReducer and add to rootReducer

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import AppLoading  from 'expo-app-loading';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer, 
-  cart: cartReducer
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
