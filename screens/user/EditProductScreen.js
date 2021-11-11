800e1b9c8fb6b1f41a581a642f7175e8c3115583
Mac Rowe <m.rowe42@gmail.com>
2021-11-11 15:44:09 -0500

    Create basic EditProductsScreen component

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const EditProductScreen = props => {
+  return (
+    <View>
+      <Text>The Edit Products Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditProductScreen;
