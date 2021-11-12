9e8479a39056cdf5a3fa0c00b2fce5a6e92ba4d9
Mac Rowe <m.rowe42@gmail.com>
2021-11-12 09:31:34 -0500

    Create basic form structure and styling

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, TextInput, StyleSheet } from 'react-native';
 
 const EditProductScreen = props => {
   return (
-    <View>
-      <Text>The Edit Products Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 20
+  },
+  formControl: {
+    width: '100%'
+  },
+  label: {
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  }
+});
 
 export default EditProductScreen;
